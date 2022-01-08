c4423ec1de838195d13d7bd78acd68a58c52df32
Takaya Ogawa <bumpofchicken344@gmail.com>
2022-01-08 22:38:54 +0900

    Add rocket

---
+++ Assets/Plugins/Utils.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils
+{
+    /// <summary>
+    /// ターゲットに向くようにY軸のみ回転
+    /// </summary>
+    public static Quaternion LookRotationY(Vector3 source, Vector3 target)
+    {
+        var lookRotation = Quaternion.LookRotation(target - source, Vector3.right);
+        //lookRotation.x = 0;
+        //lookRotation.z = 0;
+        return lookRotation;
+    }
+
+    /// <summary>
+    /// ターゲットに向くようにY軸のみ回転
+    /// </summary>
+    public static Quaternion LookRotationX(Vector3 source, Vector3 target)
+    {
+        var lookRotation = Quaternion.LookRotation(target - source, Vector3.up);
+        lookRotation.y = 0;
+        lookRotation.z = 0;
+        return lookRotation;
+    }
+}
--- Assets/Scripts/Maggellan.cs
+++ Assets/Scripts/Maggellan.cs
@@ -5,22 +5,25 @@ using static UnityEngine.Mathf;
 
 public class Maggellan : MonoBehaviour
 {
+    Transform _tf;
     Vector3 _pos = Vector3.zero;
     float _distance;
     float _theta;
-    float _velocity = 0.1f;
+    [SerializeField] float _velocity = 0.1f;
     
     void Start()
     {
-        _distance = transform.position.magnitude;
+        _tf = transform;
+        _distance = -_tf.position.magnitude;
     }
 
     void Update()
     {
         _theta += _velocity * Deg2Rad;
-        _pos.x = _distance * Cos(_theta);
-        _pos.z = _distance * Sin(_theta);
-        transform.position = _pos;
-        transform.LookAt(Vector3.zero);
+        _tf.position = new Vector3(Cos(_theta), 0f, Sin(_theta)) * _distance;
+
+        //var target = new Vector3(Cos(_theta + PI/6f), 0f, Sin(_theta + PI/6f)) * _distance;
+        //_tf.rotation = Quaternion.LookRotation(target - _tf.position, Vector3.right);
+        _tf.rotation = Utils.LookRotationY(_tf.position, Vector3.zero);
     }
 }

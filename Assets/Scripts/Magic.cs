72b030f9e8d8e9a1a2344e783d423cdc0dfe3771
Takaya Ogawa <bumpofchicken344@gmail.com>
2022-01-09 23:09:16 +0900

    Add magic

---
+++ Assets/Scripts/Magic.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magic : MonoBehaviour
+{
+    [SerializeField] SummonEffect _summonEffect;
+    [SerializeField] float _speed = 0.1f;
+    public Vector3 Direction { get; set; }
+    Transform _tf;
+
+    void OnEnable()
+    {
+        _tf = transform;
+        _tf.position = Vector3.zero;
+    }
+
+    void Update()
+    {
+        _tf.position += Direction * _speed;
+        if (_tf.position.magnitude > 40f) {
+            gameObject.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// 星座に当たったらモデルを召喚
+    /// </summary>
+    void OnTriggerEnter(Collider collider)
+    {
+        gameObject.SetActive(false);
+        collider.transform.parent.GetComponent<ZodiacSign>().Summon();
+        _summonEffect.Play();
+        Debug.Log("hit");
+    }
+}
--- Assets/Scripts/Summoner.cs
+++ Assets/Scripts/Summoner.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// 画面をタッチしたら魔法を飛ばす、星座に当たったらモデルを召喚
+/// </summary>
 public class Summoner : MonoBehaviour
 {
+    [SerializeField] Magic _magic;
     [SerializeField] SummonEffect _summonEffect;
 
     void Update()
     {
         if (!Input.GetMouseButtonDown(0)) return;
 
+        // 画面をタッチした場所に向かって魔法を飛ばす
+        _magic.gameObject.SetActive(true);
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out var hit)) {
-            Summon(hit.collider.transform.parent);
-        }
+        _magic.Direction = ray.direction;
+        //if (Physics.Raycast(ray, out var hit)) {
+        //    Summon(hit.collider.transform.parent);
+        //}
     }
 
     void Summon(Transform zodiacSign)
